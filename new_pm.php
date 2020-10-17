cef5e225d823c12fc93d46c8d27e159a9675cd22
xEkk0 <56487045+xEkk0@users.noreply.github.com>
2020-10-17 20:01:16 +0200

    Update new_pm.php

--- new_pm.php
+++ new_pm.php
@@ -6,19 +6,13 @@ include('config.php');
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="<?php echo $design; ?>/style.css" rel="stylesheet" title="Style" />
-        <link href="stylesheets/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
-        <link href="stylesheets/fontawesome/css/all.css" rel="stylesheet"/>
-		<title>New Message</title>
+		<link href="assets/style.css" rel="stylesheet" title="Style" />
+		<title>New PM</title>
 	</head>
 	<body>
-		<div class="header">
-			<a href="<?php echo $url_home; ?>"><img src="<?php echo $design; ?>/images/logo.png" alt="Members Area" /></a>
-		</div>
 
 <?php
-//We check if the user is logged
+//We check if the user is logged in
 if (isset($_SESSION['username'])) {
 	$form     = true;
 	$otitle   = '';
@@ -42,11 +36,10 @@ if (isset($_SESSION['username'])) {
 			$recip   = mysqli_real_escape_string($link, $orecip);
 			$message = mysqli_real_escape_string($link, nl2br(htmlentities($omessage, ENT_QUOTES, 'UTF-8')));
 			//We check if the recipient exists
-			$dn1 = mysqli_fetch_array(mysqli_query($link,"select count(id) as recip, id as recipid, (select count(*) from pm) as npm from users where username=$recip"));
+			$dn1 = mysqli_fetch_array(mysqli_query($link, 'select count(id) as recip, id as recipid from users where username="'.$recip.'"'));
 			if ($dn1['recip'] == 1) {
 				//We check if the recipient is not the actual user
 				if ($dn1['recipid'] != $_SESSION['userid']) {
-					$id = $dn1['npm']+1;
 					//We encrypt then send the message
 					$cipher = "aes-128-gcm";
 					$ivlen  = openssl_cipher_iv_length($cipher);
@@ -58,26 +51,25 @@ if (isset($_SESSION['username'])) {
 						$iv = openssl_random_pseudo_bytes($ivlen);
 						$ciphertext_raw = openssl_encrypt($message, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv, $tag);
 						$hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
-						$ciphertext = base64_encode($iv.$hmac.$ciphertext_raw);    //store $cipher, $iv, and $tag for decryption later
-						if (mysqli_query($link, 'insert into pm (id, id2, title, user1, user2, message, timestamp, user1read, user2read, tag)values("'.$id.'", "1", "'.$title.'", "'.$_SESSION['userid'].'", "'.$dn1['recipid'].'", "'.$ciphertext.'", "'.time().'", "yes", "no", "'.$tag.'")')) {
+						$ciphertext = base64_encode($iv.$hmac.$ciphertext_raw);    // store $cipher, $hmac and $iv for decryption later
+						if (mysqli_query($link, 'insert into pm (title, sender, recipient, message, timestamp, tag) values ("'.$title.'", "'.$_SESSION['userid'].'", "'.$dn1['recipid'].'", "'.$ciphertext.'", "'.time().'", "'.$tag.'")')) {
 ?>
 		<div class="message">The message has successfully been sent.<br />
-		<a href="list_pm.php">List of my personnal messages</a></div>
+		<a href="mailbox.php">Mailbox</a></div>
 
 <?php
 							$form = false;
 						}
-						else $error = 'An error occurred while sending the message';//Otherwise, we say that an error occured
+						else $error = 'An error occurred while sending the message.';//Otherwise, we say that an error occured
 					}
 					else $error = 'Error while sending the message.';//Otherwise, we say the user cannot send a message to himself
 				}
 				else $error = 'You cannot send a message to yourself.';//Otherwise, we say the user cannot send a message to himself
 			}
-			else $error = 'The recipient does not exists.';//Otherwise, we say the recipient does not exists
+			else $error = 'The recipient does not exist.';//Otherwise, we say the recipient does not exists
 		}
-		else $error = 'A field is empty. Please fill of the fields.';//Otherwise, we say a field is empty
+		else $error = 'Please fill in all of the fields.';//Otherwise, we say a field is empty
 	}
-	elseif (isset($_GET['recip'])) $orecip = $_GET['recip'];//We get the username for the recipient if available
 
 	if ($form) {
 		//We display a message if necessary
@@ -86,44 +78,20 @@ if (isset($_SESSION['username'])) {
 		//We display the form
 ?>
 		<div class="content">
-			<h3><i class="fa fa-plus-square"> New Message</i></h3>
-			<form class="container py-2" action="new_pm.php" method="post">
-				<div class="row form-group">
-					<div class="col-4">
-					<label for="title">Title</label>
-					</div>
-					<div class="col-5">
-					<input class="form-control" type="text" value="<?php echo htmlentities($otitle, ENT_QUOTES, 'UTF-8'); ?>" id="title" name="title" />
-					</div>
-				</div>
-
-				<div class="row form-group">
-					<div class="col-4">
-					<label for="recip">Recipient<span class="small">(Username)</span></label>
-					</div>
-
-					<div class="col-5">
-					<input type="text" value="<?php echo htmlentities($orecip, ENT_QUOTES, 'UTF-8'); ?>" id="recip" name="recip" />
-					</div>
-				</div>
-
-				<div class="row form-group">
-					<div class="col-4">
-					<label for="message">Message</label>
-					</div>
-
-					<div class="col-4">
-					<textarea cols="40" rows="5" id="message" name="message"><?php echo htmlentities($omessage, ENT_QUOTES, 'UTF-8'); ?></textarea>
-					</div>
-				</div>
-				<input class="btn btn-warning" type="submit" value="Send" />
+			<h1>New PM</h1>
+			<form action="new_pm.php" method="post">
+				Please fill the following form to send a PM.<br />
+				<label for="recip">Recipient<span class="small"> (Username)</span></label><input type="text" value="<?php echo htmlentities($orecip, ENT_QUOTES, 'UTF-8'); ?>" id="recip" name="recip" /><br />
+				<label for="title">Title</label><input type="text" value="<?php echo htmlentities($otitle, ENT_QUOTES, 'UTF-8'); ?>" id="title" name="title" /><br />
+				<label for="message">Message</label><textarea cols="40" rows="5" id="message" name="message"><?php echo htmlentities($omessage, ENT_QUOTES, 'UTF-8'); ?></textarea><br />
+				<input type="submit" value="Send" />
 			</form>
 		</div>
 <?php
 	}
 }
-else echo '<div class="message">You must be logged to access this page.</div>';
+else echo '<div class="message">You must be logged in to access this page.</div>';
 ?>
-		<div class="foot"><a class="btn btn-light" href="list_pm.php"> <i class="fa fa-inbox"></i> Inbox  </a></div>
+		<div class="foot"><a href="index.php">Home</a></div>
 	</body>
 </html>
