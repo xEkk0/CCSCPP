57d60ba794d142b37fdd60812aa63329d4988afc
xEkk0 <56487045+xEkk0@users.noreply.github.com>
2020-10-17 20:05:20 +0200

    Update new_pm.php

--- new_pm.php
+++ new_pm.php
@@ -6,8 +6,11 @@ include('config.php');
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<link href="<?php echo $design; ?>/style.css" rel="stylesheet" title="Style" />
-		<title>Read a PM</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="<?php echo $design; ?>/style.css" rel="stylesheet" title="Style" />
+        <link href="stylesheets/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="stylesheets/fontawesome/css/all.css" rel="stylesheet"/>
+		<title>New Message</title>
 	</head>
 	<body>
 		<div class="header">
@@ -17,43 +20,38 @@ include('config.php');
 <?php
 //We check if the user is logged
 if (isset($_SESSION['username'])) {
-	//We check if the ID of the discussion is defined
-	if (isset($_GET['id'])) {
-		$id = intval($_GET['id']);
-		//We get the title and the narrators of the discussion
-		$req1 = mysqli_query($link, 'select title, user1, user2 from pm where id="'.$id.'" and id2="1"');
-		$dn1  = mysqli_fetch_array($req1);
-		//We check if the discussion exists
-		if (mysqli_num_rows($req1) == 1) {
-			//We check if the user have the right to read this discussion
-			if ($dn1['user1'] == $_SESSION['userid'] or $dn1['user2'] == $_SESSION['userid']) {
-				//The discussion will be placed in read messages
-				if($dn1['user1'] == $_SESSION['userid']) {
-					$u2 = $dn1['user2'];
-					mysqli_query($link, 'update pm set user1read="yes" where id="'.$id.'" and id2="1"');
-					$user_partic = 2;
-				}
-				else {
-					$u2 = $dn1['user1'];
-					mysqli_query($link, 'update pm set user2read="yes" where id="'.$id.'" and id2="1"');
-					$user_partic = 1;
-				}
-				//We get the list of the messages
-				$req2 = mysqli_query($link, 'select pm.timestamp, pm.message, users.id as userid, users.username, users.avatar, pm.user1, pm.user2, pm.tag from pm, users where pm.id="'.$id.'" and users.id=pm.user1 order by pm.id2');
-
-				//We check if the form has been sent
-				if (isset($_POST['message']) and $_POST['message'] != '') {
-					$message = $_POST['message'];
-					//We remove slashes depending on the configuration
-					if (get_magic_quotes_gpc()) $message = stripslashes($message);
-
-					//We protect the variables
-					$message = mysqli_real_escape_string($link, nl2br(htmlentities($message, ENT_QUOTES, 'UTF-8')));					
-
+	$form     = true;
+	$otitle   = '';
+	$orecip   = '';
+	$omessage = '';
+	//We check if the form has been sent
+	if (isset($_POST['title'], $_POST['recip'], $_POST['message'])) {
+		$otitle   = $_POST['title'];
+		$orecip   = $_POST['recip'];
+		$omessage = $_POST['message'];
+		//We remove slashes depending on the configuration
+		if (get_magic_quotes_gpc()) {
+			$otitle   = stripslashes($otitle);
+			$orecip   = stripslashes($orecip);
+			$omessage = stripslashes($omessage);
+		}
+		//We check if all the fields are filled
+		if ($_POST['title'] != '' and $_POST['recip'] != '' and $_POST['message'] != '') {
+			//We protect the variables
+			$title   = mysqli_real_escape_string($link, $otitle);
+			$recip   = mysqli_real_escape_string($link, $orecip);
+			$message = mysqli_real_escape_string($link, nl2br(htmlentities($omessage, ENT_QUOTES, 'UTF-8')));
+			//We check if the recipient exists
+			$dn1 = mysqli_fetch_array(mysqli_query($link, 'select count(id) as recip, id as recipid, (select count(*) from pm) as npm from users where username="'.$recip.'"'));
+			if ($dn1['recip'] == 1) {
+				//We check if the recipient is not the actual user
+				if ($dn1['recipid'] != $_SESSION['userid']) {
+					$id = $dn1['npm']+1;
+					//We encrypt then send the message
 					$cipher = "aes-128-gcm";
 					$ivlen  = openssl_cipher_iv_length($cipher);
 					$iv     = openssl_random_pseudo_bytes($ivlen);
-					$key    = getKey($_SESSION['userid'], $u2);
+					$key    = getKey($_SESSION['userid'], $dn1['recipid']);
 					$tag    = null;
 					$method = openssl_get_cipher_methods();
 					if (in_array($cipher, $method)) {
@@ -61,83 +59,71 @@ if (isset($_SESSION['username'])) {
 						$ciphertext_raw = openssl_encrypt($message, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv, $tag);
 						$hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
 						$ciphertext = base64_encode($iv.$hmac.$ciphertext_raw);    //store $cipher, $iv, and $tag for decryption later
-						//We send the message and we change the status of the discussion to unread for the recipient
-						if (mysqli_query($link, 'insert into pm (id, id2, title, user1, user2, message, timestamp, user1read, user2read, tag)values("'.$id.'", "'.(intval(mysqli_num_rows($req2))+1).'", "", "'.$_SESSION['userid'].'", "", "'.$ciphertext.'", "'.time().'", "", "", "'.$tag.'")')) {
-							if (mysqli_query($link, 'update pm set user'.$user_partic.'read="yes" where id="'.$id.'" and id2="1"')) {
+						if (mysqli_query($link, 'insert into pm (id, id2, title, user1, user2, message, timestamp, user1read, user2read, tag)values("'.$id.'", "1", "'.$title.'", "'.$_SESSION['userid'].'", "'.$dn1['recipid'].'", "'.$ciphertext.'", "'.time().'", "yes", "no", "'.$tag.'")')) {
 ?>
-								<div class="message">Your message has successfully been sent.<br />
-								<a href="read_pm.php?id=<?php echo $id; ?>">Go to the discussion</a></div>
-<?php						}
-							else { ?>
-								<div class="message">An error occurred while updating message status.<br />
-								<a href="read_pm.php?id=<?php echo $id; ?>">Go to the discussion</a></div>
-<?php						}
+		<div class="message">The message has successfully been sent.<br />
+		<a href="list_pm.php">List of my personnal messages</a></div>
+
+<?php
+							$form = false;
 						}
-						else { ?>
-								<div class="message">An error occurred in message sending.<br />
-								<a href="read_pm.php?id=<?php echo $id; ?>">Go to the discussion</a></div>
-<?php					}
+						else $error = 'An error occurred while sending the message';//Otherwise, we say that an error occured
 					}
-					else { ?>
-							<div class="message">Encryption method not supportted.<br />
-							<a href="read_pm.php?id=<?php echo $id; ?>">Go to the discussion</a></div>
-<?php				}
+					else $error = 'Error while sending the message.';//Otherwise, we say the user cannot send a message to himself
 				}
-				else {
-				//We display the messages ?>
-		<div class="content">
-			<h1><?php echo $dn1['title']; ?></h1>
-			<table class="messages_table">
-				<tr>
-					<th class="author">User</th>
-					<th>Message</th>
-				</tr>
-<?php				while ($dn2 = mysqli_fetch_array($req2)) {
-						$cipher = "aes-128-gcm";
-						$ivlen  = openssl_cipher_iv_length($cipher);
-						$key    = getKey($_SESSION['userid'], $u2);
-						$method = openssl_get_cipher_methods();
-						if (in_array($cipher, $method)) {
-							$c    = base64_decode($dn2['message']);
-							$iv   = substr($c, 0, $ivlen);
-							$hmac = substr($c, $ivlen, $sha2len=32);
-							$ciphertext_raw = substr($c, $ivlen+$sha2len);
-							$decrypted = openssl_decrypt($ciphertext_raw, $cipher, $key, $options=OPENSSL_RAW_DATA, $iv, $dn2['tag']);
-							$calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary=true);
-							if (!hash_equals($hmac, $calcmac)) $decrypted = "Message decryption integrity failed."; //PHP 5.6+ timing attack safe comparison
-						}
-						else $decrypted = "Decryption algorithm unsupported.";
-?>
-				<tr>
-					<td class="author center">
-<?php					if ($dn2['avatar'] != '') echo '<img src="'.htmlentities($dn2['avatar']).'" alt="Image Perso" style="max-width:100px;max-height:100px;" />'; ?>
-				<br /><a href="profile.php?id=<?php echo $dn2['userid']; ?>"><?php echo $dn2['username']; ?></a></td>
-					<td class="left"><div class="date">Sent: <?php echo date('m/d/Y H:i:s' ,$dn2['timestamp']); ?></div>
-							<?php echo $decrypted; ?></td>
-				</tr>
-<?php					}
-						//We display the reply form ?>
-			</table><br />
-			<h2>Reply</h2>
-			<div class="center">
-				<form action="read_pm.php?id=<?php echo $id; ?>" method="post">
-					<label for="message" class="center">Message</label><br />
-					<textarea cols="40" rows="5" name="message" id="message"></textarea><br />
-					<input type="submit" value="Send" />
-				</form>
-			</div>
-		</div>
-		<?php
-				}
+				else $error = 'You cannot send a message to yourself.';//Otherwise, we say the user cannot send a message to himself
 			}
-			else echo '<div class="message">You dont have the rights to access this page.</div>';
+			else $error = 'The recipient does not exists.';//Otherwise, we say the recipient does not exists
 		}
-		else echo '<div class="message">This discussion does not exists.</div>';
+		else $error = 'A field is empty. Please fill of the fields.';//Otherwise, we say a field is empty
 	}
-	else echo '<div class="message">The discussion ID is not defined.</div>';
+	elseif (isset($_GET['recip'])) $orecip = $_GET['recip'];//We get the username for the recipient if available
+
+	if ($form) {
+		//We display a message if necessary
+		if (isset($error)) echo '<div class="message">'.$error.'</div>';
+
+		//We display the form
+?>
+		<div class="content">
+			<h3><i class="fa fa-plus-square"> New Message</i></h3>
+			<form class="container py-2" action="new_pm.php" method="post">
+				<div class="row form-group">
+					<div class="col-4">
+					<label for="title">Title</label>
+					</div>
+					<div class="col-5">
+					<input class="form-control" type="text" value="<?php echo htmlentities($otitle, ENT_QUOTES, 'UTF-8'); ?>" id="title" name="title" />
+					</div>
+				</div>
+
+				<div class="row form-group">
+					<div class="col-4">
+					<label for="recip">Recipient<span class="small">(Username)</span></label>
+					</div>
+
+					<div class="col-5">
+					<input type="text" value="<?php echo htmlentities($orecip, ENT_QUOTES, 'UTF-8'); ?>" id="recip" name="recip" />
+					</div>
+				</div>
+
+				<div class="row form-group">
+					<div class="col-4">
+					<label for="message">Message</label>
+					</div>
+
+					<div class="col-4">
+					<textarea cols="40" rows="5" id="message" name="message"><?php echo htmlentities($omessage, ENT_QUOTES, 'UTF-8'); ?></textarea>
+					</div>
+				</div>
+				<input class="btn btn-warning" type="submit" value="Send" />
+			</form>
+		</div>
+<?php
+	}
 }
 else echo '<div class="message">You must be logged to access this page.</div>';
 ?>
-		<div class="foot"><a href="list_pm.php">Go to my personnal messages</a></div>
+		<div class="foot"><a class="btn btn-light" href="list_pm.php"> <i class="fa fa-inbox"></i> Inbox  </a></div>
 	</body>
 </html>
